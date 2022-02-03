f35cb47d88037d1543cb35912628d94f401d605d
amena-hira <com>
2022-02-03 12:57:22 +0600

    Edit user with modal

---
+++ CRUD-Modal/resources/views/CRUD/edit.blade.php
@@ -0,0 +1,86 @@
+<!-- User Form Modal -->
+<div class="modal fade" role="dialog" tabindex="-1" id="edit_modal{{$user->id}}" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Create User</h5>
+            <button type="button" class="close" data-dismiss="modal">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="py-1">
+              <form class="form" action="{{ route('ProfileUpdate',$user->id) }}" method="POST" enctype="multipart/form-data">
+                
+                @csrf
+                <div class="row">
+                  <div class="col">
+                    <div class="row">
+                      <div class="col">
+                        <div class="form-group">
+                          <label>Full Name</label>
+                          <input class="form-control" type="text" id="full_name"  name="full_name" placeholder="John Smith" value="{{ $user->full_name }}">
+                        </div>
+                      </div>
+                      <div class="col">
+                        <div class="form-group">
+                          <label>Username</label>
+                          <input class="form-control" type="text" id="username" name="username" placeholder="johnny.s" value="{{ $user->username }}" >
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col">
+                        <div class="form-group">
+                          <label>Email</label>
+                          <input class="form-control" id="email" name="email" type="text" placeholder="user@example.com" value="{{ $user->email }}">
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col mb-3">
+                        <div class="form-group">
+                          <label>About</label>
+                          <textarea class="form-control" id="about" name="about" rows="5" placeholder="My Bio">{{ $user->about }}</textarea>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-12 col-sm-6 mb-3">
+                    <div class="mb-2"><b>Change Password</b></div>
+                    <div class="row">
+                      <div class="col">
+                        <div class="form-group">
+                          <label>Current Password</label>
+                          <input class="form-control" id="current_password" name="current_password" type="password" placeholder="••••••">
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col">
+                        <div class="form-group">
+                          <label>New Password</label>
+                          <input class="form-control" id="new_password" name="new_password" type="password" placeholder="••••••">
+                        </div>
+                      </div>
+                      <div class="col">
+                        <div class="form-group">
+                          <label>Confirm <span class="d-none d-xl-inline">Password</span></label>
+                          <input class="form-control" id="confirm_password" name="confirm_password" type="password" placeholder="••••••"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col d-flex justify-content-end">
+                    <button class="btn btn-primary" type="submit">Save Changes</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
--- CRUD-Modal/resources/views/CRUD/user_list.blade.php
+++ CRUD-Modal/resources/views/CRUD/user_list.blade.php
@@ -76,7 +76,8 @@
                         <td class="text-center align-middle">
                           <div class="btn-group align-top">
                               <a href="{!! url('profile/'.$user->id )!!}" class="btn btn-sm btn-outline-secondary badge" type="button" >View</a>
-                              <button class="btn btn-sm btn-outline-secondary badge" type="button" data-toggle="modal" data-target="#user-form-modal" >Edit</button>
+                              <button class="btn btn-sm btn-outline-secondary badge" type="button" data-toggle="modal" data-target="#edit_modal{{$user->id}}" >Edit</button>
+                              @include('CRUD.edit')
                               
                               <button class="btn btn-sm btn-outline-secondary badge" type="button" ><i class="fa fa-trash"></i></button>
                           </div>
@@ -164,90 +165,7 @@
     @include('CRUD.create')
 
     <!-- User Form Modal -->
-    <div class="modal fade" role="dialog" tabindex="-1" id="user-form-modal">
-      <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">Create User</h5>
-            <button type="button" class="close" data-dismiss="modal">
-              <span aria-hidden="true">×</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <div class="py-1">
-              <form class="form" novalidate="">
-                <div class="row">
-                  <div class="col">
-                    <div class="row">
-                      <div class="col">
-                        <div class="form-group">
-                          <label>Full Name</label>
-                          <input class="form-control" type="text" name="name" placeholder="John Smith" value="John Smith">
-                        </div>
-                      </div>
-                      <div class="col">
-                        <div class="form-group">
-                          <label>Username</label>
-                          <input class="form-control" type="text" name="username" placeholder="johnny.s" value="johnny.s">
-                        </div>
-                      </div>
-                    </div>
-                    <div class="row">
-                      <div class="col">
-                        <div class="form-group">
-                          <label>Email</label>
-                          <input class="form-control" type="text" placeholder="user@example.com">
-                        </div>
-                      </div>
-                    </div>
-                    <div class="row">
-                      <div class="col mb-3">
-                        <div class="form-group">
-                          <label>About</label>
-                          <textarea class="form-control" rows="5" placeholder="My Bio"></textarea>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-12 col-sm-6 mb-3">
-                    <div class="mb-2"><b>Change Password</b></div>
-                    <div class="row">
-                      <div class="col">
-                        <div class="form-group">
-                          <label>Current Password</label>
-                          <input class="form-control" type="password" placeholder="••••••">
-                        </div>
-                      </div>
-                    </div>
-                    <div class="row">
-                      <div class="col">
-                        <div class="form-group">
-                          <label>New Password</label>
-                          <input class="form-control" type="password" placeholder="••••••">
-                        </div>
-                      </div>
-                      <div class="col">
-                        <div class="form-group">
-                          <label>Confirm <span class="d-none d-xl-inline">Password</span></label>
-                          <input class="form-control" type="password" placeholder="••••••"></div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col d-flex justify-content-end">
-                    <button class="btn btn-primary" type="submit">Save Changes</button>
-                  </div>
-                </div>
-              </form>
-
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    
   </div>
 </div>
 </div>
