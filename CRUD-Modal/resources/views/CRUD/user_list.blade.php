8efb864de998c47fbe7684e7146ad91e3407edaa
amena-hira <com>
2022-02-03 13:03:13 +0600

    Authentication for every route

--- CRUD-Modal/resources/views/CRUD/user_list.blade.php
+++ CRUD-Modal/resources/views/CRUD/user_list.blade.php
@@ -18,8 +18,8 @@
     <div class="card p-3">
       <div class="e-navlist e-navlist--active-bg">
         <ul class="nav">
-          <li class="nav-item"><a class="nav-link px-2 active" href="profile.html"><i class="fa fa-fw fa-bar-chart mr-1"></i><span>Profile</span></a></li>
-          <li class="nav-item"><a class="nav-link px-2" href="user_list.html"><i class="fa fa-fw fa-th mr-1"></i><span>User CRUD</span></a></li>
+          <li class="nav-item"><a class="nav-link px-2 active" href="{{route('home')}}"><i class="fa fa-fw fa-bar-chart mr-1"></i><span>Home</span></a></li>
+          <li class="nav-item"><a class="nav-link px-2" href="{{route('UserList')}}"><i class="fa fa-fw fa-th mr-1"></i><span>User CRUD</span></a></li>
         </ul>
       </div>
     </div>
--- CRUD-Modal/routes/web.php
+++ CRUD-Modal/routes/web.php
@@ -17,13 +17,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/profile/{id}', 'UserListController@index')->name('profile');
-Route::get('/user_list', 'UserListController@user_list')->name('UserList');
-Route::get('/delete/{id}', 'UserListController@delete')->name('UserDelete');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('/profile/{id}', 'UserListController@index')->name('profile');
+    Route::get('/user_list', 'UserListController@user_list')->name('UserList');
+    Route::get('/delete/{id}', 'UserListController@delete')->name('UserDelete');
 
-Route::post('/create', 'UserListController@create')->name('UserCreate');
-Route::post('/profile_update/{id}', 'UserListController@profile_update')->name('ProfileUpdate');
+    Route::post('/create', 'UserListController@create')->name('UserCreate');
+    Route::post('/profile_update/{id}', 'UserListController@profile_update')->name('ProfileUpdate');
+    
+});
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
